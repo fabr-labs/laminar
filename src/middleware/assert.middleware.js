dfc6c60a221d68901f2c0f6a3b226a052234b3c9
nverba <nverba@nverbas-MacBook-Pro.local>
2022-01-22 14:40:02 +0000

    Fix stack traces for test failures

--- src/middleware/assert.middleware.js
+++ src/middleware/assert.middleware.js
@@ -1,7 +1,11 @@
 const testSummary = {
   failed: [],
   passed: 0,
+  promise: null,
+  resolve: null,
+  reject: null,
 };
+
 
 const logResult = (passed, id, values) => {
   if (passed) {
@@ -19,9 +23,14 @@ export const assertMiddleware = (ctrl) => (next) => async ({ assert, ...step }) 
 
     if (assert.init) {
       return next({ fn: () => {
-        console.log('%c Running assertions:', 'color: #FEE191; font-weight: bold; font-size: 14px; margin-top: 10px; margin-bottom: 10px;');
+        testSummary.reject?.();
+        testSummary.promise = new Promise((res, rej) => {
+          testSummary.resolve = res;
+          testSummary.reject = rej;
+        });
         testSummary.failed = [];
         testSummary.passed = 0;
+        console.log('%c Running assertions:', 'color: #FEE191; font-weight: bold; font-size: 14px; margin-top: 10px; margin-bottom: 10px;');
       }});
     }
     
@@ -30,11 +39,7 @@ export const assertMiddleware = (ctrl) => (next) => async ({ assert, ...step }) 
         console.log('%c Summary:', 'color: #FEE191; font-weight: bold; font-size: 14px; margin-top: font-size: 14px; margin-top: 10px; margin-bottom: 10px;');
         console.log('%c Assertions Passed: %o', 'color: #B0D8A4; font-weight: bold; font-size: 14px; margin: 10px;', testSummary.passed);
         console.log('%c Assertions Failed: %o', 'color: #E84258; font-weight: bold; font-size: 14px; margin: 10px;', testSummary.failed.length);
-        testSummary.failed.forEach(({ id, values }) => {
-          console.groupCollapsed('%c ✗ Failed: %o %o', 'color: #E84258', id, values);
-            console.trace();
-          console.groupEnd();
-        });
+        testSummary.resolve();
       }});
     }
   }
@@ -45,22 +50,62 @@ export const assertMiddleware = (ctrl) => (next) => async ({ assert, ...step }) 
 
     if ('equal' in assert) {
       const equal = typeof assert.equal === 'function' ? assert.equal() : assert.equal;
-      logResult(response === equal, step.id, { response, equal });
+      const pass = response === equal;
+
+      logResult(pass, step.id, { response, equal });
+
+      if (!pass) {
+        testSummary.promise.then(() => {
+          console.groupCollapsed('%c ✗ Failed: %o %o', 'color: #E84258', step.id, { response, equal });
+            console.trace();
+          console.groupEnd();
+        });
+      }
     }
 
     if ('deepEqual' in assert) {
       const deepEqual = typeof assert.deepEqual === 'function' ? assert.deepEqual() : assert.deepEqual
-      logResult(JSON.stringify(response) === JSON.stringify(deepEqual), step.id, { response, deepEqual });
+      const pass = JSON.stringify(response) === JSON.stringify(deepEqual);
+
+      logResult(pass, step.id, { response, deepEqual });
+
+      if (!pass) {
+        testSummary.promise.then(() => {
+          console.groupCollapsed('%c ✗ Failed: %o %o', 'color: #E84258', step.id, { response, deepEqual });
+            console.trace();
+          console.groupEnd();
+        });
+      }
     }
 
     if ('gt' in assert) {
       const gt = typeof assert.gt === 'function' ? assert.gt() : assert.gt;
-      logResult(response > gt, step.id, { response, gt });
+      const pass = response > gt;
+
+      logResult(pass, step.id, { response, gt });
+
+      if (!pass) {
+        testSummary.promise.then(() => {
+          console.groupCollapsed('%c ✗ Failed: %o %o', 'color: #E84258', step.id, { response, gt });
+            console.trace();
+          console.groupEnd();
+        });
+      }
     }
 
     if ('lt' in assert) {
       const lt = typeof assert.lt === 'function' ? assert.lt() : assert.gt;
-      logResult(response < lt, step.id, { response, lt });
+      const pass = response < lt;
+
+      logResult(pass, step.id, { response, lt });
+
+      if (!pass) {
+        testSummary.promise.then(() => {
+          console.groupCollapsed('%c ✗ Failed: %o %o', 'color: #E84258', step.id, { response, lt });
+            console.trace();
+          console.groupEnd();
+        });
+      }
     }
   }
 
